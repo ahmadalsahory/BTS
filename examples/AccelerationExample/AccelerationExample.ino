6792f76a35d37db10dbdf35efb401c24072ca815
Ahmad <116263824+ahmadalsahory@users.noreply.github.com>
2024-07-06 12:30:39 +0300

    Add files via upload

--- examples/AccelerationExample/AccelerationExample.ino
+++ examples/AccelerationExample/AccelerationExample.ino
@@ -5,9 +5,9 @@ int PowerPercent=100; // 100% of power
 BTS TestMotor;
 
 void setup() {
-  TestMotor.Begin(2,3,5,6); // in this order r_en,l_en,rpwm,lpwm
+  TestMotor.Begin(5,6); // in this order r_en,l_en,rpwm,lpwm
 }
 
 void loop() {
-  TestMotor.Rotate(PowerPercent,10) // (power percent,Acceleration[percent/second])
+  TestMotor.Rotate(-PowerPercent,20); // (power percent,Acceleration[percent/second])
 }
